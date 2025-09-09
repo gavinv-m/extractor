bc5b5b5dc2945c7619721ed66ac6ae859c986d55
Gavin <gavinvm@protonmail.com>
2025-09-09 14:41:33 +0200

    fix: Add react-pdf text and annotation layer styles

--- extractor-client/src/Viewers/PDFViewer.jsx
+++ extractor-client/src/Viewers/PDFViewer.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
+import { useMemo } from 'react';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import 'react-pdf/dist/Page/TextLayer.css';
 
 pdfjs.GlobalWorkerOptions.workerSrc = `https://unpkg.com/pdfjs-dist@5.3.93/build/pdf.worker.min.mjs`;
 
 export default function PDFViewer({ docUrl, width = 500 }) {
   const [numPages, setNumPages] = useState(0);
   const [pageNumber, setPageNumber] = useState(1);
+
+  const file = useMemo(() => ({ url: docUrl }), [docUrl]);
 
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
@@ -22,8 +27,13 @@ export default function PDFViewer({ docUrl, width = 500 }) {
 
   return (
     <div>
-      <Document file={{ url: docUrl }} onLoadSuccess={onDocumentLoadSuccess}>
-        <Page pageNumber={pageNumber} width={width} />
+      <Document file={file} onLoadSuccess={onDocumentLoadSuccess}>
+        <Page
+          pageNumber={pageNumber}
+          width={width}
+          renderTextLayer={true}
+          renderAnnotationLayer={true}
+        />
       </Document>
 
       <p>
